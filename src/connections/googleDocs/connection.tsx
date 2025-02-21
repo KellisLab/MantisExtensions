00614a82aca7798505c5ab7d7b84d594738813a3
LucaVor <lbvoros@gmail.com>
2025-02-21 12:39:05 -0500

    Good starting stage for google docs integration

--- src/connections/googleDocs/connection.tsx
+++ src/connections/googleDocs/connection.tsx
@@ -8,19 +8,17 @@ import docsIcon from "../../../assets/docs.png";
 import { registerAuthCookies, reqSpaceCreation } from "../../driver";
 
 const acorn = require("acorn"); // This package only works with old require
+import { median } from "d3-array";
 
 const trigger = (url: string) => {
     return url.includes("docs.google.com/document/d");
 }
 
 const createSpace = async (injectUI: injectUIType, setProgress: setProgressType) => {
-    // TODO: For popping out webpages from inside Mantis, we could
-    // have it auto pop out when control-click is done, and the point
-    // has a like a name __mantis_href attr
-    
     setProgress(GenerationProgress.GATHERING_DATA);
 
-    const extractedData: any[] = [];
+    let extractedData: any[] = [];
+    let documentString = "";
 
     document.querySelectorAll('script[nonce]').forEach((script) => {
         const content = script.textContent || "";
@@ -40,30 +38,116 @@ const createSpace = async (injectUI: injectUIType, setProgress: setProgressType)
         });
 
         if (docValue) {
-            let docCode = escodegen.generate(docValue);
+            let docCode = escodegen.generate(docValue, {
+                format: {
+                    json: true
+                },
+            });
 
-            // Swap the " with ' and vice versa in the value of docCode
-            docCode = docCode.replace(/['"]/g, function (x) { return x === '"' ? "'" : '"'; });
+            const docData: any[] = JSON.parse(docCode);
+            const documentStringSlice = docData.find((param) => param.ty === "is").s;
 
-            console.log (docCode);
-
-            extractedData.push (JSON.parse(docCode));
+            documentString += documentStringSlice;
         }
     });
 
+
+    // This syntax is absolutely DISGUSTING
+    // But it's the only way to make sure we have enough splits
+    let segments = documentString.split("\n");
+
+    if (segments.length < 50) {
+        segments = documentString.split(". ");
+
+        if (segments.length < 20) {
+            throw new Error ("Document is too short to be converted into a space");
+        }
+    }
+
+    // Merge segments that are short with the previous segment if under 20% of the average length
+    const lengths = segments.map(s => s.trim().length).filter(len => len > 0);
+    const med = median(lengths) || 0;
+    const threshold = 0.5 * med;
+
+    const continuations = ["•", "-"];
+
+    let i = 1;
+    while (segments.length > 1 && i < segments.length) {
+        const segment = segments[i].trim();
+        let merge = false;
+
+        if (segment.length < threshold) merge = true;
+        if (segments.length > 100 && continuations.some(cont => segment.startsWith(cont))) merge = true;
+        if (segments.length > 100 && /^[A-Za-z0-9]+\.\s/.test(segment)) merge = true;
+
+        if (merge) {
+            segments[i - 1] += "\n" + segments[i];
+            segments.splice(i, 1);
+            i = 1;
+        } else {
+            i++;
+        }
+    }
+
+    // Remove empty segments
+    const nonEmptySegments = segments.filter((segment) => segment.trim() !== "");
+
+    nonEmptySegments.forEach (segment => {
+        const idx = extractedData.length;
+        const title = `Segment ${idx + 1}`;
+
+        extractedData.push({ title, idx, segment });
+    });
+
+    extractedData = extractedData.filter (data => data.segment.length > 0);
+
     console.log (extractedData);
 
-    setProgress(GenerationProgress.COMPLETED);
+    if (extractedData.length < 20) {
+        throw new Error ("Document is too short to be converted into a space");
+    }
 
-    const spaceId = null;
-    const createdWidget = null;
+    setProgress(GenerationProgress.CREATING_SPACE);
+
+    const spaceData = await reqSpaceCreation(extractedData, {
+        "title": "title",
+        "idx": "numeric",
+        "segment": "semantic"
+    });
+
+    setProgress(GenerationProgress.INJECTING_UI);
+
+    const spaceId = spaceData.space_id;
+    const createdWidget = await injectUI(spaceId);
+
+    setProgress(GenerationProgress.COMPLETED);
 
     return { spaceId, createdWidget };
 }
 const injectUI = async (space_id: string) => {
     await registerAuthCookies();
 
-    return null;
+    const scale = 0.75;
+
+    // Create the iframe, hidden by default
+    const iframeScalerParent = document.createElement("div");
+    iframeScalerParent.style.width = "100%";
+    iframeScalerParent.style.height = "80vh";
+    iframeScalerParent.style.border = "none";
+
+    const iframe = document.createElement("iframe");
+    iframe.src = `${process.env.PLASMO_PUBLIC_FRONTEND}/space/${space_id}`;
+    iframe.style.border = "none";
+    iframe.style.transform = `scale(${scale})`;
+    iframe.style.transformOrigin = "top left";
+    iframe.style.width = (100 / scale).toString() + "%";
+    iframe.style.height = (80 / scale).toString() + "vh";
+    iframe.style.overflow = "hidden";
+    iframeScalerParent.appendChild(iframe);
+
+    document.querySelector("#docs-editor-container").prepend (iframeScalerParent);
+
+    return iframeScalerParent;
 }
 
 export const GoogleDocsConnection: MantisConnection = {
