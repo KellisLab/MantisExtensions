e90c29bb8a56363bfcda8458e1b132b9eee61f61
LucaVor <lbvoros@gmail.com>
2025-03-29 20:02:54 -0400

    Fixed auth, made nice menu, fixed some stuff that we discussed with manolik

--- src/connection_manager.tsx
+++ src/connection_manager.tsx
@@ -5,7 +5,7 @@ import { GoogleDocsConnection } from "./connections/googleDocs/connection";
 import { GoogleScholarConnection } from "./connections/googleScholar/connection";
 import { WikipediaSegmentConnection } from "./connections/wikipediaSegment/connection";
 
-export const CONNECTIONS = [WikipediaReferencesConnection, WikipediaSegmentConnection, GoogleConnection, PubmedConnection, GoogleDocsConnection, GoogleScholarConnection];
+export const CONNECTIONS = [WikipediaSegmentConnection, WikipediaReferencesConnection, GoogleConnection, PubmedConnection, GoogleDocsConnection, GoogleScholarConnection];
 
 export const searchConnections = (url: string, ) => {
     const connections = CONNECTIONS.filter(connection => connection.trigger(url));
