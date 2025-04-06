19e331e85b9a86ce66ff202efb7aa93a420cf0e3
Zeid Alruzouq <zalruzouq@gmail.com>
2025-04-06 13:13:58 -0500

    changes

--- src/connection_manager.tsx
+++ src/connection_manager.tsx
@@ -4,9 +4,9 @@ import { PubmedConnection } from "./connections/pubmed/connection";
 import { GoogleDocsConnection } from "./connections/googleDocs/connection";
 import { GoogleScholarConnection } from "./connections/googleScholar/connection";
 import { WikipediaSegmentConnection } from "./connections/wikipediaSegment/connection";
-import { FacebookConnection } from "./connections/facebook/connection";
+import { GmailConnection } from "./connections/Gmail/connection";
 
-export const CONNECTIONS = [WikipediaSegmentConnection, WikipediaReferencesConnection, GoogleConnection, PubmedConnection, GoogleDocsConnection, GoogleScholarConnection, FacebookConnection];
+export const CONNECTIONS = [GmailConnection, WikipediaSegmentConnection, WikipediaReferencesConnection, GoogleConnection, PubmedConnection, GoogleDocsConnection, GoogleScholarConnection];
 
 export const searchConnections = (url: string, ) => {
     const connections = CONNECTIONS.filter(connection => connection.trigger(url));
