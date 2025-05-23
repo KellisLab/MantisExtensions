4ca6d698b032a0dae8d86d8296fc704eb1c97497
Griffin Consigli <griffinyash@icloud.com>
2025-05-23 17:32:48 -0400

    client_id fix

--- plasmo.config.ts
+++ plasmo.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       "identity"
     ],
     oauth2: {
-      "client_id": "643972847997-pc3e8k15pth0um15t1lu3lq5i8sume66.apps.googleusercontent.com",
+      "client_id": "643972847997-jvqlsaierktjotv6df3fuva98degkiui.apps.googleusercontent.com",
       "scopes": ["https://www.googleapis.com/auth/drive.readonly"]
     },
   }
